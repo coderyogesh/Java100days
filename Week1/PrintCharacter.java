54ce741ffb133990913364869aae667882841201
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-23 23:16:41 +0530

    Program to print Character in Java.

---
+++ Week1/PrintCharacter.java
@@ -0,0 +1,11 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class PrintCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
