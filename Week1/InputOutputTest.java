1e012ce197182e576044eb9b4e7e60a098cdca53
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-23 23:14:57 +0530

    Input Output Using Testcases in Java.

---
+++ Week1/InputOutputTest.java
@@ -0,0 +1,23 @@
+package Week1;
+
+import java.util.Scanner;
+
+//Day 2 of Code
+// Concepts of Input Output
+//Q. Given two integers represented by 'a' and 'b' ,perform a+b, a-b, a*b with Testcases variable;
+public class InputOutputTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while( t!= 0)
+        {
+            int a;
+            int b;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println((a + b)+" "+(a-b)+ " " + (a*b));
+            t--;
+        }
+
+         }
+}
