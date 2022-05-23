776c35b30c799df90c0781cbdce5bb7c0110dace
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-23 23:17:25 +0530

    Program to Print Two float in java.

---
+++ Week1/PrintFloat.java
@@ -0,0 +1,13 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class PrintFloat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+
+        System.out.println(String.format("%.22f",a)+" "+String.format("%.22f",b));
+    }
+}
