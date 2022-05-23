6124a1f3792b5f128d860378903e571515cf63eb
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-23 23:17:36 +0530

    Program to Print Two Integer in java.

---
+++ Week1/PrintInteger.java
@@ -0,0 +1,13 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class PrintInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+
+        System.out.println(a+" "+b);
+    }
+}
