43f453cab464691152c5a68d2a9d0855868cba93
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-24 23:40:28 +0530

    Favourite Number Program in java.

---
+++ Week1/FavouriteNumber.java
@@ -0,0 +1,23 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class FavouriteNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int a =sc.nextInt();
+         while (a--> 0)
+         {
+             int n = sc.nextInt();
+             int rem, count = 0;
+             while (n != 0)
+             {
+                 rem = n % 10;
+                 n = n / 10;
+                 if (rem == 5)
+                     count++;
+             }
+             System.out.println(count);
+         }
+    }
+}
