dcae529fd06c69cb713a180cd6f5e8aface22b08
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-24 23:41:08 +0530

    Program to Print Remainder in java.

---
+++ Week1/PrintRemainder.java
@@ -0,0 +1,13 @@
+package Week1;
+//Write a program to take a number as input from the user and print the remainder when the no is divided by 10.
+
+import java.util.Scanner;
+
+public class PrintRemainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int rem = a %10;
+        System.out.println(rem);
+    }
+}
