4643ee172cabc25eca2408832cb6f930e07aa84f
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-24 23:40:54 +0530

    Program to Print Last Digit in java.

---
+++ Week1/LastDigit.java
@@ -0,0 +1,13 @@
+package Week1;
+
+import java.util.Scanner;
+
+//Take an Integer as Input from the user and print last digit of the number.
+public class LastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int lastDigit = a % 10;
+        System.out.println(lastDigit);
+    }
+}
