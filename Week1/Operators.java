06aa84893ec64df288e92a99c7249d54cad2245b
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-29 00:22:16 +0530

    Finding relation between two integers.

---
+++ Week1/Operators.java
@@ -0,0 +1,20 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a>b){
+            System.out.println(a+" is Greater than "+b);
+        }
+        else if (a<b){
+            System.out.println(a+" is smaller than "+b);
+        }
+        else {
+            System.out.println(a+"is equal to"+b);
+        }
+    }
+}
