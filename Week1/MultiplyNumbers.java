b25f84bff7bf85f5ee177edc9f14168ef35710b0
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-28 22:56:24 +0530

    Program to print multiplication of two integers.

---
+++ Week1/MultiplyNumbers.java
@@ -0,0 +1,13 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mul = a*b;
+        System.out.println(mul);
+    }
+}
