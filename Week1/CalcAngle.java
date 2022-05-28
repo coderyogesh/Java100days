9992eb2f68f7cc6e9b8fcdbcde5f849d845815b2
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-28 22:55:51 +0530

    Calculate Smaller Angle between Hour hand and minute hand.

---
+++ Week1/CalcAngle.java
@@ -0,0 +1,33 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class CalcAngle {
+public static float calcAngle() {
+
+        Scanner sc = new Scanner(System.in);
+        int h= sc.nextInt();
+        int m = sc.nextInt();
+        if (h < 0 || m < 0 || h > 12 || m > 60)
+            System.out.println("Wrong Input");
+        if (h == 12)
+            h = 0;
+        if (m == 60) {
+            m = 0;
+            h += 1;
+            if (h > 12)
+                h -= 12;
+        }
+
+        int h_angle = (int) (0.5 * (h * 60 + m));
+        int m_angle = 6 * m;
+
+        float angle = Math.abs(h_angle - m_angle);
+
+        angle = Math.min(360 - angle, angle);
+        return angle;
+    }
+    public static void main(String[] args) {
+        System.out.println(calcAngle());
+    }
+}
