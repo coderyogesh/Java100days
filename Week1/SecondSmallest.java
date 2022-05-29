c6588128a665e31d9a1277087cb78bb98721ebe2
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-29 11:38:50 +0530

    Program to Print Second Smallest/Second Largest Number Between Three numbers.

---
+++ Week1/SecondSmallest.java
@@ -0,0 +1,29 @@
+package Week1;
+
+import java.util.Scanner;
+
+import static java.lang.Math.min;
+
+//Way 1 using combining all the integers and subtracting min and max from them.
+public class SecondSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+//        int max = Math.max(a,Math.max(b,c));
+//        int min = min(a, min(b,c));
+//
+//        int sec = a+b+c - min-max;
+//        System.out.println(sec);
+
+        // Way 2 using if else
+        if (a>b && a<c || a>c && a<b ){
+            System.out.println(a);
+        }
+        else if (b>c && b<a || b<c && b>a){
+            System.out.println(b);
+        }
+        else System.out.println(c);
+    }
+}
