8e74dff8ef02d241b1fce333e05783dd2b09e8ae
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-30 09:48:24 +0530

    In how many boxes can they keep their toys such that both the toys are in the same box.

--- Week1/BoxesAndToys.java
+++ Week1/BoxesAndToys.java
@@ -1,7 +1,7 @@
 package Week1;
 
 import java.util.Scanner;
-
+//Rahul and Tina want to know in how many boxes can they keep their toys such that both the toys are in the same box.
 public class BoxesAndToys {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
