adfaa185191569d30a2bf80273a669634ca76ab2
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-30 09:51:02 +0530

    Calculating  how much each item will cost, as per condition given of the discount.

---
+++ Week1/ShoppingCost.java
@@ -0,0 +1,28 @@
+package Week1;
+
+import java.util.Scanner;
+//Tina is preparing a shopping list containing NN items.
+// She knows the quantity and price of each item on the list.
+// The shop is offering a 20%20% discount on the items with a quantity of more than 100100 units.
+// Given the quantity and price of each of the NN items, your task is to calculate how much each item will cost.
+public class ShoppingCost {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        while(n>0) {
+            float q,p,r,m;
+            q=sc.nextInt();
+            p=sc.nextInt();
+            if(q>100)
+            {r= (float) (p*0.2*q);
+                m=p*q-r;
+                System.out.println(m); }
+            else
+            { r=p*q;
+                System.out.println(r);
+            } n--;
+        }
+
+        }
+    }
+
