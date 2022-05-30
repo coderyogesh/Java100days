ee9424cc0eec2f0473785dd89cebd3f8a1b3e8b8
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-30 09:49:01 +0530

    Finding the maximum number of blocks of dimension 2*1 that can be placed in a board of dimension entered by user.

---
+++ Week1/FitSquare.java
@@ -0,0 +1,17 @@
+package Week1;
+
+import java.util.Scanner;
+//Find the maximum number of blocks of dimension 2*1 that can be placed in a board of dimension entered by user.
+public class FitSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        while(a>0){
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int count = Math.abs((m*n/2));
+            System.out.println(count);
+            a--;
+        }
+    }
+}
